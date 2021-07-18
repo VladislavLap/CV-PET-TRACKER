bc7043a244a0983716b827c4a91c0cf605422edd
Oleg <olejka647@gmail.com>
2021-07-18 12:23:36 +0300

    pet_tracker 1.0

---
+++ env2.bat
@@ -0,0 +1,7 @@
+python -m pip install --upgrade pip
+python -m pip install -U pip
+pip install opencv-python
+pip install -r "C:\Program Files (x86)\Intel\openvino_2021\deployment_tools\model_optimizer\requirements_tf.txt"
+pip install -r "C:\Program Files (x86)\Intel\openvino_2021\deployment_tools\open_model_zoo\tools\downloader\requirements.in"
+pip install -r "C:\Users\Oleg\Desktop\CV-PET-TRACKER\requirements.txt"
+"C:\Program Files (x86)\Intel\openvino_2021\bin\setupvars.bat"
